212c5b2130dcaf87b59e783cff93de38486a4407
Brendan R Borgers <brendanborgers@gmail.com>
2020-01-24 02:20:22 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,6 +9,7 @@ class Person
   def get_married(person)
     self.partner = person
     if person.class != Person
+      begin
       raise PartnerError
       rescue PartnerError => error
       puts error.message
